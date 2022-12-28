5b8c2551b2b92ed1fbf08618e931c261e4df10ce
deezitheviper <amuhdavid@gmail.com>
2022-12-29 00:57:51 +0100

    seperate env file and adjustment for mongo service

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { MONGO_IP, MONGO_PASSWORD, MONGO_PORT, MONGO_USER } from './config/config.js'; 
+
+ 
+
 
 dotenv.config();
 
-
-
 const app = express();
 
+//connect DB
+
+const connectDB = async () => {
+    console.log(MONGO_USER,MONGO_PASSWORD)
+    try{
+        mongoose.set('strictQuery', false)
+    await mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+    console.log('Connected to MongoDB')
+    }catch(err){
+        console.log(err)
+    }
+}
+
 app.get('/', (req, res) => {
-    res.send('<h2>Hello World!!!</h2>');
+    res.send('<h2>Hello World!</h2>');
 })
 
 app.listen(5000, () => {
+    connectDB();
     console.log('Server listening on port 5000');
 })
